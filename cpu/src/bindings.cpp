8c1ae6e64f44da0301f3dd0fdda68c42c83be4fe
humanpose1 <sofiane.horache@mines-paristech.fr>
2020-01-06 21:09:10 +0100

    compile well and work

--- cpu/src/bindings.cpp
+++ cpu/src/bindings.cpp
@@ -5,12 +5,9 @@ using namespace pybind11::literals;
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("group_points", &group_points);
-}
 
-
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-	m.def("ball_query",
-	      &radius_search,
+    m.def("ball_query",
+	      &ball_query,
 	      "compute the radius search of a point cloud using nanoflann"
 	      "-query : a pytorch tensor of size N1 x 3,. used to query the nearest neighbors"
 	      "- support : a pytorch tensor of size N2 x 3. used to build the tree"
@@ -21,8 +18,9 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
 	      "mode=1 means a matrix of edges of size Num_edge x 2"
 	      "return a tensor of size N1 x M where M is either max_num or the maximum number of neighbors found if mode = 0, if mode=1 return a tensor of size Num_edge x 2 and return a tensor containing the squared distance of the neighbors",
 	      "query"_a, "support"_a, "radius"_a, "max_num"_a=-1, "mode"_a=0);
-	m.def("batch_ball_query",
-	      &batch_radius_search,
+
+    m.def("batch_ball_query",
+	      &batch_ball_query,
 		"compute the radius search of a point cloud for each batch using nanoflann"
 		"-query : a pytorch tensor (float) of size N1 x 3,. used to query the nearest neighbors"
 		"- support : a pytorch tensor(float) of size N2 x 3. used to build the tree"
--- cpu/src/torch_nearest_neighbors.cpp
+++ cpu/src/torch_nearest_neighbors.cpp
@@ -9,7 +9,7 @@
 
 std::pair<at::Tensor, at::Tensor> ball_query(at::Tensor query,
 					     at::Tensor support,
-					     float radius, int max_num=-1, int mode=0){
+					     float radius, int max_num, int mode){
 
 	at::Tensor out;
 	at::Tensor out_dists;
@@ -81,7 +81,7 @@ std::pair<at::Tensor, at::Tensor> batch_ball_query(at::Tensor query,
 						   at::Tensor support,
 						   at::Tensor query_batch,
 						   at::Tensor support_batch,
-						   float radius, int max_num=-1, int mode=0) {
+						   float radius, int max_num, int mode) {
 	at::Tensor out;
 	at::Tensor out_dists;
 	auto data_qb = query_batch.DATA_PTR<long>();
@@ -134,35 +134,4 @@ std::pair<at::Tensor, at::Tensor> batch_ball_query(at::Tensor query,
 					     options=options_dist);
 	}
 	return std::make_pair(out.clone(), out_dists.clone());
-}
-using namespace pybind11::literals;
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-	m.def("radius_search",
-	      &radius_search,
-	      "compute the radius search of a point cloud using nanoflann"
-	      "-query : a pytorch tensor of size N1 x 3,. used to query the nearest neighbors"
-	      "- support : a pytorch tensor of size N2 x 3. used to build the tree"
-	      "-  radius : float number, size of the ball for the radius search."
-	      "- max_num : int number, indicate the maximum of neaghbors allowed(if -1 then all the possible neighbors will be computed). "
-	      " - mode : int number that indicate which format for the neighborhood"
-	      " mode=0 mean a matrix of neighbors(-1 for shadow neighbors)"
-	      "mode=1 means a matrix of edges of size Num_edge x 2"
-	      "return a tensor of size N1 x M where M is either max_num or the maximum number of neighbors found if mode = 0, if mode=1 return a tensor of size Num_edge x 2 and return a tensor containing the squared distance of the neighbors",
-	      "query"_a, "support"_a, "radius"_a, "max_num"_a=-1, "mode"_a=0);
-	m.def("batch_radius_search",
-	      &batch_radius_search,
-		"compute the radius search of a point cloud for each batch using nanoflann"
-		"-query : a pytorch tensor (float) of size N1 x 3,. used to query the nearest neighbors"
-		"- support : a pytorch tensor(float) of size N2 x 3. used to build the tree"
-		"- query_batch : a pytorch tensor(long) contains indices of the batch of the query size N1"
-	      "NB : the batch must be sorted"
-		"- support_batch: a pytorch tensor(long) contains indices of the batch of the support size N2"
-	      "NB: the batch must be sorted"
-		"-radius: float number, size of the ball for the radius search."
-		"- max_num : int number, indicate the maximum of neaghbors allowed(if -1 then all the possible neighbors wrt the radius will be computed)."
-		"- mode : int number that indicate which format for the neighborhood"
-		"mode=0 mean a matrix of neighbors(N2 for shadow neighbors)"
-		"mode=1 means a matrix of edges of size Num_edge x 2"
-	      "return a tensor of size N1 x M where M is either max_num or the maximum number of neighbors found if mode = 0, if mode=1 return a tensor of size Num_edge x 2 and return a tensor containing the squared distance of the neighbors",
-	      "query"_a, "support"_a, "query_batch"_a, "support_batch"_a, "radius"_a, "max_num"_a=-1, "mode"_a=0);
 }
