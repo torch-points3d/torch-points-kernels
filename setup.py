b484d460cca499e0181406c01e0b9ecaa8dfaf75
Nicolas <nicolas.chaulet@gmail.com>
2020-05-22 09:58:01 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ def get_cmdclass():
 requirements = ["torch>=1.1.0"]
 
 url = "https://github.com/nicolas-chaulet/torch-points-kernels"
-__version__ = "0.6.3"
+__version__ = "0.6.4"
 setup(
     name="torch-points-kernels",
     version=__version__,
