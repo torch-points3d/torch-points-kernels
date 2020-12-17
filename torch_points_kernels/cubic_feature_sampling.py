6bbeb910d91aabcde872dd4e06489c947efc2626
Haozhe Xie <cshzxie@gmail.com>
2020-12-17 19:18:33 +0800

    Reformat code with black.

--- torch_points_kernels/cubic_feature_sampling.py
+++ torch_points_kernels/cubic_feature_sampling.py
@@ -9,16 +9,12 @@ class CubicFeatureSamplingFunction(torch.autograd.Function):
     def forward(ctx, ptcloud, cubic_features, neighborhood_size=1):
         scale = cubic_features.size(2)
         if not torch.cuda.is_available():
-            raise NotImplementedError(
-                "CPU version is not available for Cubic Feature Sampling"
-            )
+            raise NotImplementedError("CPU version is not available for Cubic Feature Sampling")
 
         point_features, grid_pt_indexes = tpcuda.cubic_feature_sampling(
             scale, neighborhood_size, ptcloud, cubic_features
         )
-        ctx.save_for_backward(
-            torch.Tensor([scale]), torch.Tensor([neighborhood_size]), grid_pt_indexes
-        )
+        ctx.save_for_backward(torch.Tensor([scale]), torch.Tensor([neighborhood_size]), grid_pt_indexes)
         return point_features
 
     @staticmethod
@@ -56,10 +52,8 @@ def cubic_feature_sampling(ptcloud, cubic_features, neighborhood_size=1):
         (B, n_pts, n_vertices, c), where n_vertices = (neighborhood_size * 2)^3
     """
     if len(ptcloud.shape) != 3 or ptcloud.shape[2] != 3:
-        raise ValueError('The input point cloud should be of size (B, n_pts, 3).')
+        raise ValueError("The input point cloud should be of size (B, n_pts, 3).")
 
     h_scale = cubic_features.size(2) / 2
     ptcloud = ptcloud * h_scale + h_scale
-    return CubicFeatureSamplingFunction.apply(
-        ptcloud, cubic_features, neighborhood_size
-    )
+    return CubicFeatureSamplingFunction.apply(ptcloud, cubic_features, neighborhood_size)
