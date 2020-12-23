88dfbd542b1c4275012025c4b3708711ec0ce904
Nicolas Chaulet <nicolas.chaulet@gmail.com>
2020-12-23 09:34:12 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ def get_cmdclass():
 requirements = ["torch>=1.1.0", "numba", "scikit-learn"]
 
 url = "https://github.com/nicolas-chaulet/torch-points-kernels"
-__version__ = "0.6.9"
+__version__ = "0.6.10"
 setup(
     name="torch-points-kernels",
     version=__version__,
